783f4d2c8a8e71cdb2cd72f30dee76a5705826b6
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2020-11-04 18:42:45 +0100

    Commit initial

---
+++ server.php
@@ -0,0 +1,71 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+//On recupere la methode de la resuete
+$method = $_SERVER['REQUEST_METHOD'];
+
+//Si la met est de type GET et que la session ne contient pas de USER
+if ($method === 'GET' && empty($_SESSION['user'])) {
+
+    //On est redirigé vers la page de login.php
+    header('Location: http://multiplygame.as/pages/login.php');
+    exit();
+}
+
+require_once 'functions/helper.php';
+
+//GERENATION ALEATOIRE DES FACTEURS
+if (!empty($_GET['update'])) {
+    try {
+        $facteurs['a'] = random_int(10, 99);
+        $facteurs['b'] = random_int(10, 99);
+        echo json_encode($facteurs, JSON_THROW_ON_ERROR);
+    } catch (Exception $e) {
+        $e->getMessage();
+    }
+}
+
+//INSERTION EN BDD DES TENTATIVES
+if (!empty($_POST['tentative'])) {
+
+    $operation = $_POST['operation'];
+    $reponse = (int)$_POST['reponse'];
+    $statut = $_POST['statut'] === "true" ? 1 : 0;
+    $pseudo = $_POST['pseudo'];
+
+    $connexion = db_connexion();
+
+    $sql = "insert into tentatives(operation, reponse, statut, pseudo) values (?, ?, ?, ?)";
+
+    try {
+        $req_preparee = $connexion->prepare($sql);
+        $resutalt = $req_preparee->execute([$operation, $reponse, $statut, $pseudo]);
+    } catch (Exception $e) {
+        $e->getMessage();
+    } finally {
+        $connexion = null;
+    }
+}
+
+//SELECTIONNE LES TENTATIVES D'UN USER
+if (!empty($_GET['all'])) {
+
+    $pseudo = $_GET['pseudo'];
+
+    $connexion = db_connexion();
+
+    $sql = "select * from tentatives where pseudo = ?";
+
+    try {
+        $req_preparee = $connexion->prepare($sql);
+        $req_preparee->execute([$pseudo]);
+        $tentatives = $req_preparee->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($tentatives, JSON_THROW_ON_ERROR);
+    } catch (PDOException | JsonException $e) {
+        $e->getMessage();
+    } finally {
+        $connexion = null;
+    }
+}
